fd463f2404bf8196e12e304d28be895ff73d4645
Calvin Pelletier <calvin.pelletier@gmail.com>
2019-03-27 15:48:52 -0700

    .

--- live_cnn.py
+++ live_cnn.py
@@ -16,6 +16,9 @@ from models import *
 from data_preprocessor import TorchDataset, load_mean_std_dev
 from util import get_crop
 
+# NOTE: dont forget to append "dwc_otg.fiq_fsm_mask=0x3" to /boot/cmdline.txt (same line)
+# https://www.raspberrypi.org/forums/viewtopic.php?f=28&t=197089
+
 IS_RPI = os.environ['IS_RASPBERRYPI'].lower() == 'true'
 
 if IS_RPI:
@@ -37,13 +40,17 @@ THRESHOLD = 1.
 # THRESHOLD = 100.
 
 ACTIVE_START = 750
-ACTIVE_END = 830
-ALWAYS_ACTIVE = True
+ACTIVE_END = 815
+ALWAYS_ACTIVE = False
 
 SAVE_START = 730
 SAVE_END = 800
 SAVE_EVERY_N_INTERVALS = 3 # 0 for no save
-ALWAYS_SAVE = True
+ALWAYS_SAVE = False
+
+LIGHT_START = 740
+LIGHT_END = 800
+ALWAYS_LIGHT = False
 
 DEBUG = False
 
@@ -66,7 +73,6 @@ def live_data_preprocessor():
 
     return x_live, y_live
 
-
 model = c5pc5pc5pfn(c1=5,c2=10,c3=10,f1=100)
 if IS_RPI:
     model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
@@ -84,22 +90,13 @@ while 1:
     cur_datetime = datetime.datetime.now()
     int_time = int(cur_datetime.strftime("%H%M"))
 
-    if ALWAYS_ACTIVE or (int_time >= ACTIVE_START and int_time < ACTIVE_END) or (int_time >= SAVE_START and int_time < SAVE_END):
+    if ALWAYS_LIGHT or (int_time >= LIGHT_START and int_time < LIGHT_END):
         if not light_on:
             if IS_RPI:
                 light.on()
             else:
                 subprocess.check_call('light on', shell=True)
             light_on = True
-
-        if IS_RPI:
-            subprocess.check_call('sudo streamer -f jpeg -o {}'.format(IMG_PATH), shell=True)
-        else:
-            subprocess.check_call('streamer -f jpeg -o {}'.format(IMG_PATH), shell=True)
-        if SAVE_EVERY_N_INTERVALS != 0 and (int_time >= SAVE_START and int_time < SAVE_END or ALWAYS_SAVE):
-            if count % SAVE_EVERY_N_INTERVALS == 0:
-                shutil.copyfile(IMG_PATH, os.path.join(UNLABELED_PATH, cur_datetime.strftime("%Y-%m-%d-%H-%M-%S") + '.jpeg'))
-            count += 1
     else:
         if light_on:
             if IS_RPI:
@@ -107,6 +104,20 @@ while 1:
             else:
                 subprocess.check_call('light off', shell=True)
             light_on = False
+
+    if ALWAYS_ACTIVE or (int_time >= ACTIVE_START and int_time < ACTIVE_END) \
+                     or (int_time >= SAVE_START and int_time < SAVE_END):
+        subprocess.check_call('fswebcam -r 320x240 --no-banner {}'.format(IMG_PATH), shell=True)
+        if SAVE_EVERY_N_INTERVALS != 0 and (int_time >= SAVE_START and int_time < SAVE_END or ALWAYS_SAVE):
+            if count % SAVE_EVERY_N_INTERVALS == 0:
+                shutil.copyfile(
+                    IMG_PATH,
+                    os.path.join(
+                        UNLABELED_PATH,
+                        cur_datetime.strftime("%Y-%m-%d-%H-%M-%S") + '.jpeg'
+                    )
+                )
+            count += 1
 
     if ALWAYS_ACTIVE or (int_time >= ACTIVE_START and int_time < ACTIVE_END):
         x_live, y_live = live_data_preprocessor()
@@ -124,11 +135,15 @@ while 1:
         if confidence > THRESHOLD and not active:
             print('launching alarm')
             active = True
-            alarm_process = subprocess.Popen('mplayer {} -loop 0'.format(ALARM_PATH), stdout=subprocess.PIPE,
-                       shell=True, preexec_fn=os.setsid)
+            alarm_process = subprocess.Popen('mplayer {} -loop 0'.format(ALARM_PATH),
+                stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
         elif confidence < THRESHOLD and active:
             print('stopping alarm')
             active = False
             os.killpg(os.getpgid(alarm_process.pid), signal.SIGTERM)
+    elif active:
+        print('stopping alarm')
+        active = False
+        os.killpg(os.getpgid(alarm_process.pid), signal.SIGTERM)
 
     time.sleep(INTERVAL)
